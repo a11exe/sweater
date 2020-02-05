83ddc7c36cc7e9c63563e8bb23cc694a384f4d75
alabra <alllexe@mail.ru>
2020-02-05 16:32:07 +0300

    added turbolinks

---
+++ src/main/java/com/alllexe/sweater/util/RedirectInterceptor.java
@@ -0,0 +1,25 @@
+package com.alllexe.sweater.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * @author Alexander Abramov (alllexe@mail.ru)
+ * @version 1
+ * @since 05.02.2020
+ */
+public class RedirectInterceptor extends HandlerInterceptorAdapter {
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+      ModelAndView modelAndView) throws Exception {
+
+    if (modelAndView != null) {
+      String args = request.getQueryString() != null ? request.getQueryString() : "";
+      String url = request.getRequestURI().toString() + "?" + args;
+      response.setHeader("TurboLinks-Location", url);
+    }
+  }
+}
